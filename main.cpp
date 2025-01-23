d23c949b188c352fdba3953201d3fd788c555c2a
Ярослав <144372173+YaroslavBojko@users.noreply.github.com>
2025-01-23 12:44:49 +0300

    data entry

--- main.cpp
+++ main.cpp
@@ -6,4 +6,12 @@ int main()
 {
 	setlocale(LC_ALL, "Rus");
 
+	int firstNumber;
+	cout << "Введите первое число: ";
+	cin >> firstNumber;
+	int secondNumber;
+	cout << "Введите второе число: ";
+	cin >> secondNumber;
+	cout << "-----Проверяем-----" << endl;
+
 }
